39e581b83fbb1d05ae8694cae424fc9fc7b497a9
jc9820 <cy9820@gmail.com>
2021-06-30 09:23:01 +0800

    Update chenyu.js

--- js/chenyu.js
+++ js/chenyu.js
@@ -15,10 +15,10 @@
           event.returnValue = false;
 
       //询问框
-      layer.confirm('<p>最怕问初衷，大梦成空。</p><p>眉间鬓上老英雄，剑甲鞮鍪封厚土，说甚擒龙。 </p><p>壮志付西风，逝去无踪。</p><p>少年早作一闲翁，诗酒琴棋终日里，岁月匆匆。</p>', {
+      layer.confirm('<div style="padding:20px; line-height:30px; color: #000; text-align:center;"><p>最怕问初衷，大梦成空。</p><p>眉间鬓上老英雄，剑甲鞮鍪封厚土，说甚擒龙。 </p><p>壮志付西风，逝去无踪。</p><p>少年早作一闲翁，诗酒琴棋终日里，岁月匆匆。</p></div>', {
         btn: ['对不起','寒山别哭'] //按钮
       }, function(){
-        layer.msg('<p>我怀念的不是哪个人，而是我那回不去的曾经，</p><p>突然很想跟自己说声对不起，对不起再也找不回原来的自己了。</p>', {icon: 1});
+        layer.msg('<div style="padding:20px; line-height:30px; color: #000; text-align:center;"><p>我怀念的不是哪个人，而是我那回不去的曾经，</p><p>突然很想跟自己说声对不起，对不起再也找不回原来的自己了。</p></div>', {icon: 1});
       }, function(){
         layer.msg('我说寒山别哭，你说嘴巴嘟嘟……', {
           time: 20000, //20s后自动关闭
