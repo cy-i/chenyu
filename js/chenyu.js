882e7a481f88f86d003d8b58604ef632da6a3b32
jc9820 <cy9820@gmail.com>
2021-07-02 16:13:20 +0800

    Update chenyu.js

--- js/chenyu.js
+++ js/chenyu.js
@@ -148,28 +148,3 @@ function aidaori(){
         return 0;
     }
 }
-
-/* 字体闪烁变色特效 */
-function magicColor(mode,t){
-    t=t||10;
-    let elem=document.getElementsByClassName("magic-color");
-    if(!elem){
-        setTimeout(function(){
-            magicColor(mode,t-1);
-        },400);
-        return;
-    }
-    if(window.mcHandler==undefined){
-        window.mcHandler={elements:[]};
-        window.mcHandler.colorIndex=0;
-        window.mcHandler.run=function(mode){
-            let color=mode=="random"?("rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+",1)"):["#CC0000","#9999CC","#CC3366","#669999","#FFCC00","#00CCCC","#CC00CC"][(window.mcHandler.colorIndex++)%7];
-            for(var i=0,L=window.mcHandler.elements.length;i<L;i++)window.mcHandler.elements[i].style.color=color;
-        }
-    }
-    window.mcHandler.elements=elem;
-    if(window.mcHandler.timer==undefined){
-        window.mcHandler.timer=setInterval(()=>{window.mcHandler.run(mode)},500);
-    }
-}
-magicColor(random);//random为随机颜色，否则为固定颜色随机；上方“闪烁（变色）”字样为固定颜色随机
