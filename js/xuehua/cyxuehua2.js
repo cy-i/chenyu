721f90ddcf4facf32a4fa61c9bb114adf3c4a395
jc9820 <cy9820@gmail.com>
2021-07-06 14:02:12 +0800

    修改雪花特效

---
+++ js/xuehua/cyxuehua2.js
@@ -0,0 +1,9 @@
+// See sources on GitHub:
+// https://github.com/mkalygin/snowflakes
+
+var snowConfig = {
+  color: [161 / 256, 197 / 256, 231 / 256],
+  count: 1000,
+  opacity: 0.8,
+  density: 1 / 50,
+};
