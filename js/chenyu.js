701e60b090d0bbdfa81c45cc7e2558a2825cf85a
jc <cy9820@gmail.com>
2022-07-04 16:37:17 +0800

    Update chenyu.js
    取消指定日期里网站变灰色的设置

--- js/chenyu.js
+++ js/chenyu.js
@@ -108,47 +108,6 @@
 }
 });
 
-/* 哀悼日网站变灰 */
-if(aidaori()){
-    $("html").css({
-        "filter":"gray !important",
-        "filter":"progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)",
-        "filter":"grayscale(100%)",
-        "-webkit-filter":"grayscale(100%)",
-        "-moz-filter":"grayscale(100%)",
-        "-ms-filter":"grayscale(100%)",
-        "-o-filter":"grayscale(100%)" 
-    });
-}
-
-function aidaori(){
-    var aidaoriarr=new Array("0403","0404","0405","0406","0414","0512","0807","0909","1213");
-    //2020年4月4日 新冠肺炎哀悼日，清明节
-    //2010年4月14日，青海玉树地震
-    //2008年5月12日，四川汶川地震
-    //2010年8月7日，甘肃舟曲特大泥石流
-    //1976年9月9日，毛泽东逝世
-    //1937年12月13日，南京大屠杀
-    var mydate = new Date();
-    var str = "";// + mydate.getFullYear();
-    var mm = mydate.getMonth()+1;
-    if(mydate.getMonth()>9){
-      str += mm;
-    }else{
-      str += "0" + mm;
-    }
-    if(mydate.getDate()>9){
-      str += mydate.getDate();
-    }else{
-      str += "0" + mydate.getDate();
-    }
-    if(aidaoriarr.indexOf(str)>-1){
-        return 1;
-    }else{
-        return 0;
-    }
-}
-
 /* 尘遇-公告 */
 function cookiesave(n, v, mins, dn, path)
 {
