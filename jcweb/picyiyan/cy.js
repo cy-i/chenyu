a6cf516d3bcd9227b9f0f6fe8c3954315dcd825a
jc <cy9820@gmail.com>
2022-09-21 19:33:21 +0800

    新增图片古诗一言代码
    新增图片古诗一言代码的JS代码和图片

---
+++ jcweb/picyiyan/cy.js
@@ -0,0 +1,35 @@
+var wiiuiiYiYan = [
+  '休对故人思故国，且将新火试新茶。诗酒趁年华。——宋苏轼《望江南》',
+  '山中何事？松花酿酒，春水煎茶。——元张可久《人月圆》',
+  '最是人间留不住，朱颜辞镜花辞树。——清·王国维《蝶恋花》',
+  '正是江南好风景，落花时节又逢君。——唐·杜甫《江南逢李龟年》',
+  '醉后不知天在水，满船清梦压星河。——元·唐温如《题龙阳县青草湖》',
+  '纸屏石枕竹方床，手倦抛书午梦长。——宋蔡确《夏日登车盖亭》',
+  '曾是惊鸿照影来。——宋陆游《沈园二首》',
+  '人生天地间，忽如远行客。——汉佚名《古诗十九首》',
+  '直道相思了无益，未妨惆怅是清狂。——唐·李商隐《无题》',
+  '秋风生渭水，落叶满长安。——唐·贾岛《忆江上吴处士》',
+  '一愿郎君千岁，二愿妾身常健。——唐冯延巳《长命女》',
+  '雾失楼台，月迷津渡。桃源望断无寻处。——宋秦观《踏莎行郴州旅舍》',
+  '独立小楼风满袖，平林新月人归后。——南唐冯延巳《鹊踏枝》',
+  '晓看天色暮看云，行也思君，坐也思君。——明唐寅《一剪梅》',
+  '叹息老来交旧尽，睡来谁共午瓯茶。——宋陆游《幽居初夏》',
+  '客散酒醒深夜后，更持红烛赏残花。——唐李商隐《花下醉》',
+  '柴门闻犬吠，风雪夜归人。——唐·刘长卿《逢雪宿芙蓉山主人》',
+  '庭院深深深几许，杨柳堆烟，帘幕无重数。——宋欧阳修《蝶恋花》',
+  '有约不来过夜半，闲敲棋子落灯花。——宋赵师秀《约客》',
+  '满目山河空念远，落花风雨更伤春。不如怜取眼前人。——宋晏殊《浣溪沙》'
+]
+var wiiuiiYiYanBg = [
+    'https://cdn.azpay.cn/2022731/images/2.png',
+    'https://cdn.azpay.cn/2022731/images/4.png',
+    'https://cdn.azpay.cn/2022731/images/5.png',
+    'https://cdn.azpay.cn/2022731/images/6.png',
+    'https://cdn.azpay.cn/2022731/images/7.png',
+    'https://cdn.azpay.cn/2022731/images/8.png',
+    'https://cdn.azpay.cn/2022731/images/10.png',
+    'https://cdn.azpay.cn/2022731/images/11.png',
+    'https://cdn.azpay.cn/2022731/images/12.png',
+    'https://cdn.azpay.cn/2022731/images/14.png'
+    ]
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 9=2.3(".j-I-J"),k=9.l.l,7=K L,4=7.M()+1,b=7.N();2.3(".O").5=7.P()+"\\Q";10>4?2.3(".4").5="0"+4+"\\m":2.3(".4").5=4+"\\m";10>4&&(2.3(".b").5="0"+b);k.o.p="0";6 q=2.3("#d-R-S"),e=2.3(".d-r");f g(){6 a=8.s(8.t()*u.v);e.5=w[8.s(8.t()*w.v)];9.o.T="U("+u[a]+")";V.W("\\n %c \\X\\Y\\Z\\11\\12\\13 - x://14.j.y","15:#16; 17: 18-19(1a, #z 0%, #z A%, #B h(A% + i), #B C%, #D h(C% + i), #D E%, #F h(E% + i), #F 1b%);p:G; 1c-1d: G;")}g();q.H=f(){g()};e.H=f(){6 a=2.3(".d-r").1e.1f("\\1g")[0];1h("x://1i.1j.y/1k.1l?1m="+a)};',62,85,'||document|querySelector|wiiuiiMonth|innerHTML|var|date|Math|wiiuiiYiMain||wiiuiiDay||xingyu|wiiuiiYiYinTextBox|function|wiiuiiRanYiYin|calc|1px|wiiuii|wiuiSjMain|parentNode|u6708||style|padding|wiiuiiYyRanBtn|yiyin|floor|random|wiiuiiYiYanBg|length|wiiuiiYiYan|https|cn|212121|40|323232|60|008F95|70|14FFEC|5px|onclick|suiji|main|new|Date|getMonth|getDate|wiiuiiYear|getFullYear|u5e74|qh|btn|backgroundImage|url|console|log|u661f|u8bed|u7684||u5c0f|u6728|u5c4b|www|color|fff|background|linear|gradient|55deg|100|border|radius|textContent|split|u3002|open|so|gushiwen|search|aspx|value'.split('|'),0,{}))
