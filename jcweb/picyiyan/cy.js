610b8bf36eb56f536b78c29ab409bf1182036300
jc <cy9820@gmail.com>
2022-09-22 12:50:05 +0800

    Update cy.js

--- jcweb/picyiyan/cy.js
+++ jcweb/picyiyan/cy.js
@@ -1,4 +1,4 @@
-var wiiuiiYiYan = [
+var cymeYiYan = [
   '休对故人思故国，且将新火试新茶。诗酒趁年华。——宋苏轼《望江南》',
   '山中何事？松花酿酒，春水煎茶。——元张可久《人月圆》',
   '最是人间留不住，朱颜辞镜花辞树。——清·王国维《蝶恋花》',
@@ -20,16 +20,37 @@ var wiiuiiYiYan = [
   '有约不来过夜半，闲敲棋子落灯花。——宋赵师秀《约客》',
   '满目山河空念远，落花风雨更伤春。不如怜取眼前人。——宋晏殊《浣溪沙》'
 ]
-var wiiuiiYiYanBg = [
-    'https://cdn.azpay.cn/2022731/images/2.png',
-    'https://cdn.azpay.cn/2022731/images/4.png',
-    'https://cdn.azpay.cn/2022731/images/5.png',
-    'https://cdn.azpay.cn/2022731/images/6.png',
-    'https://cdn.azpay.cn/2022731/images/7.png',
-    'https://cdn.azpay.cn/2022731/images/8.png',
-    'https://cdn.azpay.cn/2022731/images/10.png',
-    'https://cdn.azpay.cn/2022731/images/11.png',
-    'https://cdn.azpay.cn/2022731/images/12.png',
-    'https://cdn.azpay.cn/2022731/images/14.png'
+var cymeYiYanBg = [
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/2.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/4.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/5.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/6.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/7.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/8.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/10.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/11.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/12.webp',
+    'https://cdn.jsdelivr.net/gh/cy-j/chenyu@1.8.8/jcweb/picyiyan/14.webp'
     ]
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 9=2.3(".j-I-J"),k=9.l.l,7=K L,4=7.M()+1,b=7.N();2.3(".O").5=7.P()+"\\Q";10>4?2.3(".4").5="0"+4+"\\m":2.3(".4").5=4+"\\m";10>4&&(2.3(".b").5="0"+b);k.o.p="0";6 q=2.3("#d-R-S"),e=2.3(".d-r");f g(){6 a=8.s(8.t()*u.v);e.5=w[8.s(8.t()*w.v)];9.o.T="U("+u[a]+")";V.W("\\n %c \\X\\Y\\Z\\11\\12\\13 - x://14.j.y","15:#16; 17: 18-19(1a, #z 0%, #z A%, #B h(A% + i), #B C%, #D h(C% + i), #D E%, #F h(E% + i), #F 1b%);p:G; 1c-1d: G;")}g();q.H=f(){g()};e.H=f(){6 a=2.3(".d-r").1e.1f("\\1g")[0];1h("x://1i.1j.y/1k.1l?1m="+a)};',62,85,'||document|querySelector|wiiuiiMonth|innerHTML|var|date|Math|wiiuiiYiMain||wiiuiiDay||xingyu|wiiuiiYiYinTextBox|function|wiiuiiRanYiYin|calc|1px|wiiuii|wiuiSjMain|parentNode|u6708||style|padding|wiiuiiYyRanBtn|yiyin|floor|random|wiiuiiYiYanBg|length|wiiuiiYiYan|https|cn|212121|40|323232|60|008F95|70|14FFEC|5px|onclick|suiji|main|new|Date|getMonth|getDate|wiiuiiYear|getFullYear|u5e74|qh|btn|backgroundImage|url|console|log|u661f|u8bed|u7684||u5c0f|u6728|u5c4b|www|color|fff|background|linear|gradient|55deg|100|border|radius|textContent|split|u3002|open|so|gushiwen|search|aspx|value'.split('|'),0,{}))
+
+var cymeYiMain = document.querySelector(".cyme-suiji-main"),
+    wiuiSjMain = cymeYiMain.parentNode.parentNode,
+    date = new Date,
+    cymeMonth = date.getMonth() + 1,
+    cymeDay = date.getDate();
+document.querySelector(".cymeYear").innerHTML = date.getFullYear() + "年";
+10 > cymeMonth ? document.querySelector(".cymeMonth").innerHTML = cymeMonth + "月" : document.querySelector(".cymeMonth").innerHTML = cymeMonth + "月";
+10 > cymeMonth && (document.querySelector(".cymeDay").innerHTML = cymeDay);
+wiuiSjMain.style.padding = "0";
+var cymeYyRanBtn = document.querySelector("#xingyu-qh-btn"),
+    cymeYiYanTextBox = document.querySelector(".xingyu-YiYan");
+
+function cymeRanYiYan() {
+    var a = Math.floor(Math.random() * cymeYiYanBg.length);
+    cymeYiYanTextBox.innerHTML = cymeYiYan[Math.floor(Math.random() * cymeYiYan.length)];
+    cymeYiMain.style.backgroundImage = "url(" + cymeYiYanBg[a] + ")";
+}
+cymeRanYiYan();
+cymeYyRanBtn.onclick = function() {
+    cymeRanYiYan()
+};
