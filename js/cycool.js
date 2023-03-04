513130a60f53d7c322b4fb2dd33f2617a9f18d77
cy-i <yuc@outlook.my>
2023-03-04 12:11:30 +0800

    新增cycool动态标题
    新增cycool动态标题js

---
+++ js/cycool.js
@@ -0,0 +1,17 @@
+var OriginTitile = document.title,
+titleTime;
+document.addEventListener("visibilitychange",
+function() {
+if (document.hidden) {
+  $('[rel="shortcut icon"]').attr("href", "https://cdn.jsdelivr.net/gh/cy-i/chenyu/img/logo/cycool.ico");
+  document.title = "江湖走马，风也好，雨也罢";
+  clearTimeout(titleTime)
+} else {
+  $('[rel="shortcut icon"]').attr("href", "https://cdn.jsdelivr.net/gh/cy-i/chenyu/yu.ico");
+  document.title = "得鹿梦鱼 - " + OriginTitile;
+  titleTime = setTimeout(function() {
+      document.title = OriginTitile
+  },
+  2000)
+}
+});
